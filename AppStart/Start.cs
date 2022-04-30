ab91daf60a5f7a0fdb99139943ed2119735505b7
Anastasija Draskovic <draskovic.sv67.2020@uns.ac.rs>
2022-04-30 12:18:15 +0200

    Added SecretaryUI, SecretaryController, CRUD is done, and blocking patients

--- AppStart/Start.cs
+++ AppStart/Start.cs
@@ -11,6 +11,7 @@ using HealthcareSystem.Entity.HealthCardModel;
 using MongoDB.Bson;
 using HealthcareSystem.Functions;
 using HealthcareSystem.UI;
+using HealthcareSystem.RoleControllers;
 
 namespace HealthcareSystem.AppStart;
 
@@ -21,47 +22,67 @@ static class Start
 
     public static void ProgramStart()
     {
-    
 
-  
+
+
         var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Tim17:UXGhApWVw9oc6VGg@cluster0.se6mz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
         settings.ServerApi = new ServerApi(ServerApiVersion.V1);
         var client = new MongoClient(settings);
         var database = client.GetDatabase("USI");
+        UserController uc = new UserController(database);
+        HealthCardController hc = new HealthCardController(database);
 
+        Console.WriteLine(uc.userCollection);
 
         User loggedUser = null;
-
-        while (loggedUser == null) {
-            Console.WriteLine("############Hospital: Novak Djokovic####################");
-            Console.Write("Enter e-mail(or x for exti): ");
-            string email = Console.ReadLine();
-            if(email == "x")
+        Boolean notLogged = true;
+        while (notLogged) {
+            Console.WriteLine();
+            Console.WriteLine("WELCOME TO OUR HEALTHCARE SYSTEM");
+            Console.WriteLine("1 -> Log in");
+            Console.WriteLine("2  -> Exit");
+            Console.WriteLine("Choose option: ");
+            string choice = Console.ReadLine();
+            if (choice.Equals("1"))
             {
-                loggedUser = null;
+                Console.Write("Enter e-mail: ");
+                string email = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Enter password: ");
+                string password = Console.ReadLine();
+                Console.WriteLine();
+                loggedUser = Login.Validate(database, email, password);
+
+
+                if (loggedUser != null)
+                {
+                    Console.WriteLine("HELLO!");
+                    Console.WriteLine("You are logged in as: " +  loggedUser.role.ToString().ToUpper() +  "  ->  " + loggedUser.name + " " + loggedUser.lastName);
+                    Console.WriteLine();
+                    if (loggedUser.role == Role.MANAGER)
+                    {
+                        ManagerUI ui = new ManagerUI(database, loggedUser);
+                        loggedUser = null;
+                    }if (loggedUser.role == Role.SECRETARY) 
+                    {
+                        SecretaryControllers secretaryControllers = new SecretaryControllers(database);
+                        SecretaryUI ui = new SecretaryUI(secretaryControllers, loggedUser);
+                        loggedUser = null;
+                        notLogged = true;
+                    }
+
+                   
+                }
+            }
+            else if (choice.Equals("2")){
                 break;
             }
-            Console.WriteLine();
-            Console.Write("Enter password: ");
-            string password = Console.ReadLine();
-
-            loggedUser = Login.validate(database, email, password);
-
 
         }
 
-
-        if (loggedUser != null)
-        {
-            Console.WriteLine(loggedUser.name);
-
-
-            if (loggedUser.role == Role.MANAGER)
-            {
-                ManagerUI ui = new ManagerUI(database, loggedUser);
-                loggedUser = null;
-            }
-        }
     }
 
-}
+   
+
+    }
+
--- Functions/Login.cs
+++ Functions/Login.cs
@@ -5,24 +5,28 @@ using MongoDB.Driver;
 namespace HealthcareSystem.Functions;
 
 static class Login {
-    public static User validate(IMongoDatabase db,string email,string password) {
+    public static User Validate(IMongoDatabase db,string email,string password) {
         var userCollection = db.GetCollection<User>("Users");
         var doctorCardCollection = db.GetCollection<Doctor>("Doctors");
         UserController uc = new UserController(db);
         DoctorController dc = new DoctorController(db);
 
 
-        User loggedUser = uc.checkCredentials(email, password);
+        User loggedUser = uc.CheckCredentials(email, password);
         BlockedUserController blockedUserController = new BlockedUserController(db);
 
 
         if (loggedUser == null)
         {
             loggedUser = (User)dc.checkCredentials(email, password);
+            if (loggedUser == null) {
+                Console.WriteLine("Your email/password are incorrect! Please try again! ");
+                return null;
+            }
         }
         if (loggedUser != null) {
-            if (blockedUserController.checkIfBlocked(loggedUser._id) != null) {
-                Console.WriteLine("Sorry, you are blocked!B)");
+            if (blockedUserController.CheckIfBlocked(loggedUser._id) != null) {
+                Console.WriteLine("Sorry, you are blocked! ");
                 return null;
             }
         }
